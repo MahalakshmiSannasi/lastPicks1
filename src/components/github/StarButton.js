e4b2b7ad809fd5b154388fe616c7de82699373a8
Preethi Krishnan <preethi.krishnan@verizon.com>
2018-07-04 07:11:23 +0000

    Landing page

---
+++ src/components/github/StarButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StarButton = () => (
+  <div className="star-button-container">
+    <a className="github-button" href="https://github.com/jeffersonRibeiro/react-shopping-cart" data-size="large" aria-label="Star jeffersonRibeiro/react-shopping-cart on GitHub">Register/Login</a>
+  </div>
+);
+
+export default StarButton;
+
